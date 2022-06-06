2605647ad498fd92de63cf6b56520a78a69acde3
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-06 22:40:11 +0300

    add sort & egipet

--- scripts/main.js
+++ scripts/main.js
@@ -23,17 +23,32 @@ console.log('c = ', c);
 
 buttonlink.addEventListener('click', (ev) => {
     console.log('Стороны: ', a, b, c);
-if (((a+b)<c) || ((a+c)<b) || ((b+c)<a)) {
-    console.log('Треугольник построить нельзя!');
-    return;
-}
-if ((a === b) && (b === c) && (a === c)) {
-    console.log('Треугольник равносторонний');
-    return;
-}
-if ((a === b) || (a === c) || (b === c)) {
-    console.log('Треугольник равнобедренный'); 
-    return;
-}
-console.log('ПРОШЛИ');
+
+    if (((a+b)<c) || ((a+c)<b) || ((b+c)<a)) {
+        console.log('Треугольник построить нельзя!');
+        return;
+    }
+
+    if ((a === b) && (b === c) && (a === c)) {
+        console.log('Треугольник равносторонний');
+        return;
+    }
+
+    if ((a === b) || (a === c) || (b === c)) {
+        console.log('Треугольник равнобедренный'); 
+        return;
+    }
+
+    let d; //for change big or small
+    if (a > b) {console.log('a = ', a,' > b = ', b, 'меняем!'); d = b; b = a; a = d;}
+    if (b > c) {console.log('b = ', b,' > c = ', c, 'меняем!'); d = c; c = b; b = d;}
+    if (a > c) {console.log('a = ', a,' > c = ', c, 'меняем!'); d = c; c = a; a = d;}
+    if (a > b) {console.log('a = ', a,' > b = ', b, 'меняем!'); d = b; b = a; a = d;}
+    console.log('Стороны теперь: ', a, b, c);
+
+    if ((a/3 === b/4) && (b/4 === c/5)) {
+        console.log('Треугольник египетский');
+        return;
+    }
+    console.log('Вы построили обычный треугольник.');
 });
